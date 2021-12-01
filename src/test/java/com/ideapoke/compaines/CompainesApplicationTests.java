93245fa9e95ab76b96f4542e45fff7a3a8f976d7
arjunsuvarna1 <arjunsuvarna.s@gmail.com>
2021-12-01 21:49:40 +0530

    inital commit

---
+++ src/test/java/com/ideapoke/compaines/CompainesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideapoke.compaines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompainesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
